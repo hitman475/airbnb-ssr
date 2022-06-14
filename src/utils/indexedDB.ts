18dadd59808a00b29da14e10e7f4824c41b9647d
hitman475 <1353206432@qq.com>
2022-06-14 22:57:26 +0800

    chapter4 content 1

---
+++ src/utils/indexedDB.ts
@@ -0,0 +1,106 @@
+export default class DB {
+    // 数据库名称
+    private dbName: string
+    // 创建事务对象
+    private db: any
+
+    constructor(dbName: string) {
+        this.dbName = dbName
+    }
+
+    // 打开数据库
+    public openStore(storeName:string, keyPath:string, index?:Array<string>) {
+        const request = window.indexedDB.open(this.dbName, 1)
+        request.onsuccess = (event) => {
+            this.db = event.target.result
+            console.log('数据库打开成功')
+            console.log(event)
+        }
+
+        request.onerror = (event) => {
+            console.log('数据库打开失败')
+            console.log(event)
+        }
+
+        request.onupgradeneeded = (event) => {
+            console.log('数据库更新成功')
+            // 创建对象仓库
+            const { result } :any = event.target
+            const store = result.createObjectStore(storeName, { autoIncrement: true, keyPath })
+            if (index && index.length > 0) {
+                index.map((v:string) => {
+                     store.createIndex(v, v, { unique: false })
+                })
+            }
+            store.transaction.oncomplete = (event:any) => {
+                console.log('创建对象仓库成功')
+            }
+            console.log(event)
+        }
+    }
+
+    // 增、改数据
+    updateItem(storeName:string, data:any) {
+        // param1:表名 param2:要传入的数据
+        const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
+        const request = store.put({
+            ...data,
+            updateTime: new Date()  // 给对象添加一个时间戳属性，放置因为值相同而插入失败
+        })
+        request.onsuccess = (event: any) => {
+            console.log('更新数据成功')
+            console.log(event)
+        }
+
+        request.onerror = (event: any) => {
+            console.log('更新数据失败')
+            console.log(event)
+        }
+    }
+
+    // 删除数据
+    deletItem(storeName:string, key: number | string) {
+        // param1:表名 param2:要传入的数据
+        const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
+        const request = store.delete(key)
+        request.onsuccess = (event: any) => {
+            console.log('删除数据成功')
+            console.log(event)
+        }
+
+        request.onerror = (event: any) => {
+            console.log('删除数据失败')
+            console.log(event)
+        }
+    }
+
+    // 查询所有数据
+    getList(storeName:string) {
+        const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
+        const request = store.getAll()
+        request.onsuccess = (event: any) => {
+            console.log('查询所有数据成功')
+            console.log(event.target.result)
+        }
+
+        request.onerror = (event: any) => {
+            console.log('查询所有数据失败')
+            console.log(event)
+        }
+    }
+
+    // 查询单条数据
+    getItem(storeName:string, key: number | string) {
+        const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
+        const request = store.get(key)
+        request.onsuccess = (event: any) => {
+            console.log('查询数据成功')
+            console.log(event.target.result)
+        }
+
+        request.onerror = (event: any) => {
+            console.log('查询数据失败')
+            console.log(event)
+        }
+    }
+}
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,54 @@
 <script setup lang="ts">
+import IndexDB from '@/utils/indexedDB.ts'
 import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
+
+const indexDB = new IndexDB('aribnb')
+indexDB.openStore('elephant', 'id', ['nose', 'ear'])
+
+// 增加数据
+function addDB(storeName:string) {
+    indexDB.updateItem(storeName, {
+        nose: '55m',
+        ear: 'small'
+    })
+}
+
+// 修改数据
+function updateDB(storeName:string) {
+    indexDB.updateItem(storeName, {
+        id: 1,
+        nose: '55m',
+        ear: 'small'
+    })
+}
+
+// 删除数据
+function deleteDB(storeName:string, key: number | string) {
+    indexDB.deletItem(storeName, key)
+}
+
+// 查询所有数据
+function getDBAllData(storeName:string) {
+    indexDB.getList(storeName)
+}
+
+// 查询单条数据
+function getSingleData(storeName:string, key:number|string) {
+    indexDB.getItem(storeName, key)
+}
 
 </script>
 
 <template>
     {{ t('message.home') }}
+    <el-button @click="addDB('elephant')">增</el-button>
+    <el-button @click="updateDB('elephant')">改</el-button>
+    <el-button @click="deleteDB('elephant', 2)">删除</el-button>
+    <el-button @click="getDBAllData('elephant')">查所有</el-button>
+    <el-button @click="getSingleData('elephant', 1)">查</el-button>
+
+
 
 </template>
 
